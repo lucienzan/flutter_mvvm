035f8e6cd05ff46510b79678c79094f6c197d4fc
Lucienzan <changwilliam826@gmail.com>
2024-10-15 19:08:48 +0630

    [main] #default

---
+++ lib/config/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import '../screens/home/home.dart';
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Home(),
+      ),
+    );
+  }
+}
